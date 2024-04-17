bbeac82a2214c86deebaa3a622972614892c5dcf
emblalundqvist <embla@dotvoid.se>
2024-04-17 13:13:02 +0200

    ändringar

--- login/register.js
+++ login/register.js
@@ -6,11 +6,18 @@ function renderRegisterContainer (parentid) {
             <input id='username' placeholder='Användarnamn' type='text'>
             <input id='password' placeholder='Lösenord' type='password'>
             <input id='password' placeholder='Bekräfta lösenord' type='password'>
-            <button id='register-button'>Logga in</button>
+            <button id='register-button'>Skapa konto</button>
             <p class='subtitle-register'>Har du redan ett konto?</p>
             <a href='./login.html'>Logga in</a>
         </div>
         `;
+
+        const registerButton = document.getElementById('register-button');
+        registerButton.addEventListener('click', (e) => {
+
+        })
 }
+
+
 
 renderRegisterContainer();
