e60d49f1f9e9e88297b6e1392c8ca8f74884f3a2
emblalundqvist <embla@dotvoid.se>
2024-04-17 15:12:26 +0200

    login added

---
+++ login/login.js
@@ -0,0 +1,19 @@
+
+
+function renderLoginContainer () {
+   let loginWrapper = document.getElementById('wrapper-login');
+   loginWrapper.innerHTML = `
+            <div id='login-Container'>
+           <h1>LOGGA IN</h1>
+           <input id='login-username' placeholder='Användarnamn' type='text'>
+           <input id='login-password' placeholder='Lösenord' type='password'>
+           <button id='login-button'>Logga in</button>
+           <p class='subtitle-login'>Har du inte ett konto?</p>
+           <a href='./register.html'>Skapa konto</a>
+       </div>
+       `;
+}
+
+
+renderLoginContainer();
+
