e07680bd6b77abccd2621fe78184eb84fe8ff869
emblalundqvist <embla@dotvoid.se>
2024-04-19 13:51:16 +0200

    fixade sökvägar till nav

--- nav/nav.js
+++ nav/nav.js
@@ -2,11 +2,11 @@ function renderNavContainer(parentId) {
     const parent = parentId;
     const navContainer = document.createElement('nav');
     navContainer.innerHTML = `
-            <a href='./../DU3-projekt/index.html'>HEM</a>
-            <a href='./../DU3-projekt/recept/recept.html'>RECEPT</a>
-            <a href='./../DU3-projekt/minsida/minsida.html'>MIN SIDA</a>
-            <a href='./../DU3-projekt/index.html'>OM OSS</a>
-            <a href='./../DU3-projekt/login/login.html'>LOGGA IN</a>
+            <a href='./../index.html'>HEM</a>
+            <a href='./../recipes/recipes.html'>RECEPT</a>
+            <a href='./../my_page/my_page.html'>MIN SIDA</a>
+            <a href='./../index.html'>OM OSS</a>
+            <a href='./../login_register/login.html'>LOGGA IN</a>
             `;
 
     parent.append(navContainer);
