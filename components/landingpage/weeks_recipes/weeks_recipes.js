b18561dd72bedd7ebf09ac3817f494dc322bc92f
oliviagalazka <olivia.galazka02@gmail.com>
2024-04-25 17:34:03 +0200

    försökte lösa random recipes

--- components/landingpage/weeks_recipes/weeks_recipes.js
+++ components/landingpage/weeks_recipes/weeks_recipes.js
@@ -1,11 +1,11 @@
-function renderDaysRecipes (parentId) {
+function renderDaysRecipes(parentId) {
     const parent = parentId;
     const weeksRecipesContainer = document.createElement('div');
     weeksRecipesContainer.id = 'weeksRecipesContainer'
     weeksRecipesContainer.innerHTML = `
         <h2>Dagens Recept</h2>
         <div id='dagens-recept-container'>
-
+        ${getRandomRecipes(4)}
         </div>
     `;
 
---
+++ components/recipes/recipes_random/recipes_random.js
@@ -0,0 +1,58 @@
+"use strict";
+
+/*
+async function getRandomRecipes(amount) {
+    await State.Get({
+        entity: 'recipes', request: './../../api/recipes.php'
+    });
+
+    const recipes = State.GetEntity('recipes');
+    console.log(recipes);
+
+    for (let i = 0; i < amount; i++) {
+        const random_recipe = array_random_element(recipes);
+        console.log(random_recipe);
+        //renderRecipesCard('weeksRecipesContainer', random_recipe)
+        // console.log(random_recipe);
+    }
+}
+*/
+
+function get_random_number(max, min = 0) {
+    // Returns a random number between min (inclusive) and max (exclusive)
+
+    return min + Math.floor(max * Math.random());
+}
+
+function array_random_element(a) {
+    const random_index = get_random_number(a.length);
+    return a[random_index];
+}
+
+
+async function getRandomRecipes(amount) {
+    await State.Get({
+        entity: 'recipes', request: './../../api/recipes.php'
+    });
+
+    const recipes = State.GetEntity('recipes');
+    console.log(recipes);
+
+    // Skapa en kopia av listan med recept
+    const shuffledRecipes = recipes.slice();
+
+    // Blanda om listan med recept
+    for (let i = shuffledRecipes.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledRecipes[i], shuffledRecipes[j]] = [shuffledRecipes[j], shuffledRecipes[i]];
+    }
+
+    // Välj de första "amount" recepten från den blandade listan
+    const selectedRecipes = shuffledRecipes.slice(0, amount);
+
+    selectedRecipes.forEach(random_recipe => {
+        console.log(random_recipe);
+        //renderRecipesCard('weeksRecipesContainer', random_recipe)
+        //console.log(random_recipe);
+    });
+}
