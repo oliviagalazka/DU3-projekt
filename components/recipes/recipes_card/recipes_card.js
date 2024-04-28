56dfe9ed012b11dc4789207e93a282a111367a17
oliviagalazka <olivia.galazka02@gmail.com>
2024-04-29 00:03:36 +0200

    Update recipes_card.js

--- components/recipes/recipes_card/recipes_card.js
+++ components/recipes/recipes_card/recipes_card.js
@@ -1,5 +1,5 @@
 
-async function renderRecipeCard(parentID, recipe) {
+function renderRecipeCard(parentID, recipe) {
     const parent = document.getElementById(parentID);
     const recipeCard = document.createElement('div');
 
@@ -10,45 +10,16 @@ async function renderRecipeCard(parentID, recipe) {
         renderRecipePopup(recipe);
     });
 
-    const review = await recipeAveregeReview(recipe);
-
     recipeCard.innerHTML = `
         <div id='rc-img'>Bild</div>
         <div id='rc-info'>
             <h3 id='rc-name'>${recipe.name}</h3>
             <div id='rc-review-heart'>
-                <p id='rc-review'>${review.averageRank}/10 (av ${review.totalReviews} omdömen)</p>
+                <p id='rc-review'>9/10 (av 15 omdömen)</p>
                 <div id='rc-heart'>&#x2661</div>
             </div>
         </div>
     `;
 
     parent.append(recipeCard);
-}
-
-async function recipeAveregeReview(recipe) {
-    await State.Get({
-        entity: 'reviews', request: './../../api/reviews.php'
-    });
-    const reviews = State.GetEntity('reviews');
-
-    const allReviews = [];
-    for (let review of reviews) {
-        if (review.recipeId === recipe.id) {
-            allReviews.push(review.rank);
-        }
-    }
-
-    let totalRank = 0;
-    for (let rank of allReviews) {
-        totalRank += rank;
-    }
-
-    const averageRank = totalRank / allReviews.length;
-
-    if (allReviews.length === 0) {
-        return { averageRank: "-", totalReviews: allReviews.length };
-    } else {
-        return { averageRank: averageRank, totalReviews: allReviews.length };
-    }
 }
