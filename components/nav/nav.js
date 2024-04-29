e2ef6e4b718376a9b0e2eaf67bca87e7736619bb
oliviagalazka <olivia.galazka02@gmail.com>
2024-04-29 21:18:41 +0200

    Update nav.js

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -5,9 +5,17 @@ function renderNavContainer(parentId) {
             <a href='./../../index.html'>HEM</a>
             <a href='./components/recipes/recipes.html'>RECEPT</a>
             <a href='./components/mypage/mypage.html'>MIN SIDA</a>
-            <a href='./../../index.html'>OM OSS</a>
+            <a href='./../../index.html' id='about-us'>OM OSS</a>
             <a href='./components/login/login.html'>LOGGA IN</a>
             `;
 
     parent.append(navContainer);
+
+    document.getElementById('about-us').addEventListener('click', (e) => {
+        e.preventDefault();
+        const section = document.getElementById('AboutUsContainer');
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
 }
