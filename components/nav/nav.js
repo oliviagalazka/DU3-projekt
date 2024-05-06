b5fd72c777f0d60d5c4ea3d2e9e16be153be58c0
oliviagalazka <olivia.galazka02@gmail.com>
2024-05-06 21:56:39 +0200

    fixade css med nav och att man inte ser min sida om man ej är inloggad

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -2,32 +2,36 @@ function renderNavContainer(parentId) {
     const parent = document.getElementById(parentId);
     const navContainer = document.createElement('nav');
 
-
     if (window.localStorage.length !== 0) {
         navContainer.innerHTML = `
-        <a href='./../../index.html'>HEM</a>
-        <a href='./components/recipes/recipes.html'>RECEPT</a>
-        <a href='./components/mypage/mypage.html'>MIN SIDA</a>
-        <a href='./../../index.html' id='about-us'>OM OSS</a>
-        <a href='./../../index.html' id="logout-button">LOGGA UT</a>
-         `;
+                                <div>
+                                    <a href='./../../index.html'>HEM</a>
+                                    <a href='./components/recipes/recipes.html'>RECEPT</a>
+                                    <a href='./components/mypage/mypage.html'>MIN SIDA</a>
+                                    <a href='./../../index.html' id='about-us'>OM OSS</a>
+                                </div>
+                                <div>
+                                    <a href='./../../index.html' id="logout-button">LOGGA UT</a>
+                                </div>
+                                `;
 
         parent.append(navContainer);
 
         const logoutButton = document.getElementById('logout-button');
         logoutButton.addEventListener('click', async () => {
             window.localStorage.removeItem('login');
-        })
-
-
+        });
     } else {
         navContainer.innerHTML = `
-        <a href='./../../index.html'>HEM</a>
-        <a href='./components/recipes/recipes.html'>RECEPT</a>
-        <a href='#'>MIN SIDA</a>
-        <a href='./../../index.html' id='about-us'>OM OSS</a>
-        <a href='./components/login/login.html'>LOGGA IN</a>
-        `;
+                                <div>
+                                    <a href='./../../index.html'>HEM</a>
+                                    <a href='./components/recipes/recipes.html'>RECEPT</a>
+                                    <a href='./../../index.html' id='about-us'>OM OSS</a>
+                                </div>
+                                <div>
+                                    <a href='./components/login/login.html'>LOGGA IN</a>
+                                </div>
+                                `;
         parent.append(navContainer);
     }
 
