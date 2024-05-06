3915ce5cfd9d8082a1866010f3b57e79ddf40fd4
oliviagalazka <olivia.galazka02@gmail.com>
2024-05-06 15:42:35 +0200

    Update about_us.js

--- components/landingpage/about_us/about_us.js
+++ components/landingpage/about_us/about_us.js
@@ -8,10 +8,10 @@ function renderAboutUs(parentId) {
             <p>Välkomna till NamNam – vår egen lilla kokbok online! Vi är ett härligt gäng bestående av fyra studenter från Malmö Universitet som sedan hösten 2023 studerar programmet Medieteknik: Webbbaserad Design och Utveckling. Tillsammans har vi tagit på oss uppdraget att skapa en plattform där alla kan hitta inspiration och enkelhet i köket då matlagning är en av våra gemensamma intressen.</p>
             <div>
                 <p>Möt vårt team:</p>
-                <p>Olivia: Webbutvecklare och webbdesigner. Favoritmaträtt: </p>
-                <p>[Klasskamrat 1]: Webbutvecklare och webbdesigner. Favoritmaträtt: </p>
-                <p>[Klasskamrat 2]: Webbutvecklare och webbdesigner. Favoritmaträtt: </p>
-                <p>[Klasskamrat 3]: Webbutvecklare och webbdesigner. Favoritmaträtt: </p>
+                <p>Olivia: Bild. Favoritmaträtt: </p>
+                <p>[Klasskamrat 1]: Bild. Favoritmaträtt: </p>
+                <p>[Klasskamrat 2]: Bild. Favoritmaträtt: </p>
+                <p>[Klasskamrat 3]: Bild. Favoritmaträtt: </p>
             </div>
             <p>Tillsammans strävar vi efter att förena vår passion för teknik med vår kärlek till matlagning. Genom att kombinera våra kunskaper och erfarenheter hoppas vi kunna erbjuda en inspirerande och användarvänlig plattform där alla kan hitta recept som passar deras smak och kunskapsnivå i köket. Tack för att du besöker vår sida, och vi hoppas att du hittar massor av smakrika upplevelser här!</p>
         </div>
