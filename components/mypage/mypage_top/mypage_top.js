b8eef84ee9c947bf31b857d71d361ece7c69a78b
oliviagalazka <olivia.galazka02@gmail.com>
2024-05-06 14:57:44 +0200

    text på mypage

--- components/mypage/mypage_top/mypage_top.js
+++ components/mypage/mypage_top/mypage_top.js
@@ -4,8 +4,14 @@ function renderMyPageTop(parentId) {
     myPageTopContainer.id = 'mypage-top-container';
     myPageTopContainer.innerHTML = `
                                     <div id='my-page-info'>
-                                        <h1>Min Sida</h1>
-                                        <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+                                        <h1>MIN SIDA</h1>
+                                        <p>Hej kära medlem!</p>
+                                        <p>Du som har ett konto hos oss NamNam har möjlighet att:</p>
+                                        <ul>
+                                            <li>Skapa din helt egna inköpslista<br>Håll koll på vilka ingredienser du behöver handla hem för att kunna laga de recept du längtat efter att testa.</li>
+                                            <li>Spara dina favoritrecept<br>Samla alla dina favoritrecept på en plats, så att det är enkelt att hitta tillbaka till dem igen.</li>
+                                        </ul>
+                                        <p>Allt relaterat till detta kan du hitta på denna sida!</p>
                                     </div>
                                     <div id='shopping-list'>
                                     <div>Inköpslista</div>
