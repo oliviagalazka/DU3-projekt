7f725961ea7a4b83762d07509e008ca7a3b5f5c3
emblalundqvist <embla@dotvoid.se>
2024-05-07 11:36:24 +0200

    bakgrund och bakgrundsskugga div

--- components/mypage/mypage_top/mypage_top.js
+++ components/mypage/mypage_top/mypage_top.js
@@ -3,21 +3,23 @@ function renderMyPageTop(parentId) {
     const myPageTopContainer = document.createElement('div');
     myPageTopContainer.id = 'mypage-top-container';
     myPageTopContainer.innerHTML = `
-                                    <div id='my-page-info'>
-                                        <h1>MIN SIDA</h1>
-                                        <p>Hej kära medlem!</p>
-                                        <p>Du som har ett konto hos oss NamNam har möjlighet att:</p>
-                                        <ul>
-                                            <li>Skapa din helt egna inköpslista<br>Håll koll på vilka ingredienser du behöver handla hem för att kunna laga de recept du längtat efter att testa.</li>
-                                            <li>Spara dina favoritrecept<br>Samla alla dina favoritrecept på en plats, så att det är enkelt att hitta tillbaka till dem igen.</li>
-                                        </ul>
-                                        <p>Allt relaterat till detta kan du hitta på denna sida!</p>
-                                    </div>
-                                    <div>
-                                    <div id='shopping-list'>Inköpslista</div>
-                                    <div>textgubbe</div>
-                                    </div>
-                                    `;
+    <div id='shadow'></div>
+        <div id='my-page-info'>
+            <h1>MIN SIDA</h1>
+            <p>Hej kära medlem!</p>
+            <p>Du som har ett konto hos oss NamNam har möjlighet att:</p>
+            <ul>
+                <li>Skapa din helt egna inköpslista<br>Håll koll på vilka ingredienser du behöver handla hem för att kunna laga de recept du längtat efter att testa.</li>
+                <li>Spara dina favoritrecept<br>Samla alla dina favoritrecept på en plats, så att det är enkelt att hitta tillbaka till dem igen.</li>
+            </ul>
+            <p>Allt relaterat till detta kan du hitta på denna sida!</p>
+        </div>
+        <div id='shopping-list-container'>
+            <div id='shopping-list'>Inköpslista</div>
+            <div>textgubbe</div>
+        </div>
+    </div>
+    `;
 
     parent.append(myPageTopContainer);
 
