71dbbd99c1c811945a0fdb9cca67fb7144b720bc
emblalundqvist <embla@dotvoid.se>
2024-05-07 11:00:11 +0200

    footer css

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -3,8 +3,14 @@ function renderFooterContainer(parentId) {
     const footerContainer = document.createElement("footer");
 
     footerContainer.innerHTML = `
-        <div>LOGGA</div>
-        <div>copyright text</div>
+        <div>
+            <div id='img-footer-text-logo'>
+                <div id='img-footer-logo'></div>
+                <h2>NAMNAM</h2>
+            </div>
+        
+            <p>copyright text</p>
+        </div>
     `
     parent.append(footerContainer);
 }
