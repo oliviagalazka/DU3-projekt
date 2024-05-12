1510c4d243650fa869b167c96755b044b6f02d6e
hildahammer <hilda_hammer@yahoo.se>
2024-05-12 21:22:36 +0200

    renskrev mypage

--- components/mypage/mypage_top/mypage_top.js
+++ components/mypage/mypage_top/mypage_top.js
@@ -1,17 +1,19 @@
 function renderMyPageTop(parentId) {
     const parent = document.getElementById(parentId);
     const myPageTopContainer = document.createElement('div');
+    let username = localStorage.getItem('login').toUpperCase();
     myPageTopContainer.id = 'mypage-top-container';
     myPageTopContainer.innerHTML = `
     <div id='shadow'></div>
         <div id='my-page-info'>
             <h1>MIN SIDA</h1>
-            <p>Kära medlem!</p>
-            <p>Du som har ett konto hos NamNam har möjlighet att:</p>
+            <p id="mypage-user">HEJ ${username}!</p>
+            <p> Du som har ett konto hos NamNam har möjlighet att: <p>
             <ul>
-                <li>Skapa din helt egna inköpslista<br>Håll koll på vilka ingredienser du behöver handla hem för att kunna laga de recept du längtat efter att testa.</li>
-                <li>Spara dina favoritrecept<br>Samla alla dina favoritrecept på en plats, så att det är enkelt att hitta tillbaka till dem igen.</li>
+                <li>Skapa personliga inköpslistor: Håll reda på vilka ingredienser du behöver för att tillaga dina favoritrecept.</li>
+                <li>Spara dina favoritrecept: Samla dina mest älskade recept på en plats för enkel åtkomst när inspirationen slår till.</li>
             </ul>
+
         </div>
         <div id='shopping-list-container'>
             <div id='shopping-list'></div>
