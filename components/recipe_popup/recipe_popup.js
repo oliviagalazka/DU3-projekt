eb9af57090ccab80cc9392dec3d74e4fefc05fb9
emblalundqvist <embla@dotvoid.se>
2024-05-12 11:00:39 +0200

    pillat med popupen

--- components/recipe_popup/recipe_popup.js
+++ components/recipe_popup/recipe_popup.js
@@ -2,18 +2,25 @@ function renderRecipePopup(recipe) {
   const recipePopupWrapper = document.createElement('div');
   recipePopupWrapper.id = 'recipe-popup-wrapper';
 
+  const recipePopupContent = document.createElement('div');
+  recipePopupContent.id = 'recipe-popup-content';
+
   // Stäng popup knapp
   const closePopupButton = document.createElement('a');
   closePopupButton.id = 'closePopupButton';
   closePopupButton.innerHTML = '྾';
   closePopupButton.addEventListener('click', (e) => {
     e.preventDefault();
+    document.body.classList.remove('prevent-scroll');
     recipePopupWrapper.remove();
   });
 
-  recipePopupWrapper.appendChild(closePopupButton);
+  recipePopupContent.appendChild(closePopupButton);
+  recipePopupWrapper.appendChild(recipePopupContent);
+
+  document.body.classList.add('prevent-scroll');
   document.body.appendChild(recipePopupWrapper);
 
-  renderRecipeSection('recipe-popup-wrapper', recipe);
-  renderReviewSection('recipe-popup-wrapper', recipe);
-};
+  renderRecipeSection('recipe-popup-content', recipe);
+  renderReviewSection('recipe-popup-content', recipe);
+}
